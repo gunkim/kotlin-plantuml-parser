c8627a19b83ccdc7e26e47c4f8af3bb0a2d59433
gunkim <gunkim.dev@gmail.com>
2023-03-05 00:55:48 +0900

    :recycle: 구조 리팩토링

--- core/src/main/kotlin/io/github/babiesdev/domain/plantuml/PumlClass.kt
+++ core/src/main/kotlin/io/github/babiesdev/domain/plantuml/PumlClass.kt
@@ -2,6 +2,16 @@ package io.github.babiesdev.domain.plantuml
 
 class PumlClass(
     private val name: String,
+    private val basePackage: PumlPackage,
     private val fields: List<PumlField>,
     private val methods: List<PumlMethod>,
-)
+    private val visibility: Visibility = Visibility.PUBLIC,
+) {
+    fun toPlantUml() = """
+            |${visibility.symbol}class ${basePackage.name}.${name} {
+            |   ${fields.joinToString(separator = "") { it.toPlantUml() }}
+            |   --
+            |   ${methods.joinToString(separator = "") { it.toPlantUml() }}
+            |}
+    """.trimMargin()
+}
--- core/src/main/kotlin/io/github/babiesdev/domain/plantuml/PumlMethod.kt
+++ core/src/main/kotlin/io/github/babiesdev/domain/plantuml/PumlMethod.kt
@@ -1,7 +1,13 @@
 package io.github.babiesdev.domain.plantuml
 
+import io.github.babiesdev.domain.plantuml.Visibility.PUBLIC
+
 class PumlMethod(
     private val name: String,
-    private val returnType: String,
+    private val returnType: String = "Unit",
     private val parameters: List<PumlField>,
-)
+    private val visibility: Visibility = PUBLIC,
+) {
+    fun toPlantUml() =
+        "${visibility.symbol} fun $name(${parameters.joinToString(separator = ", ") { it.toPlantUml() }}):$returnType"
+}
