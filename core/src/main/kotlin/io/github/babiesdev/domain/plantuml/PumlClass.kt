94d4f643f9be8e8dd2b8bf349bab745d440bb94e
gunkim <gunkim.dev@gmail.com>
2023-03-08 22:31:56 +0900

    :recycle: 파싱 로직 수정

--- core/src/main/kotlin/io/github/babiesdev/domain/plantuml/PumlClass.kt
+++ core/src/main/kotlin/io/github/babiesdev/domain/plantuml/PumlClass.kt
@@ -2,18 +2,19 @@ package io.github.babiesdev.domain.plantuml
 
 class PumlClass(
     private val name: String,
+    private val type: PumlClassType = PumlClassType.CLASS,
     private val basePackage: PumlPackage,
     private val fields: List<PumlField>,
     private val methods: List<PumlMethod>,
-    private val relation: PumlRelation,
+    private val relation: PumlRelation? = null,
     private val visibility: Visibility = Visibility.PUBLIC,
 ) {
     fun toPlantUml() = """
-            |${visibility.symbol}class ${basePackage.name}.${name} {
+            |${visibility.symbol}${type.symbol} ${basePackage.name}.${name} {
             |  ${fields.joinToString(separator = "\n  ") { it.toPlantUml() }}
             |  --
             |  ${methods.joinToString(separator = "\n  ") { it.toPlantUml() }}
             |}
-            |${relation.toPlantUml("${basePackage.name}.${name}")}
+            |${relation?.toPlantUml("${basePackage.name}.${name}") ?: ""}
     """.trimMargin()
 }
